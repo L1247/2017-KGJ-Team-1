9de088769d98a3d23dfc03a5245be9de44a52db9
L1247 <starlove124760@gmail.com>
2017-06-17 15:11:08 +0800

    場景 資源上傳

---
+++ Assets/Scripts/Player/PlayerGravity.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerGravity : MonoBehaviour
+{
+        public PlanetGravity planetGravity;
+
+        private Transform m_transform;
+        Rigidbody rigibody;
+
+        void Awake ()
+        {
+                rigibody = GetComponent<Rigidbody>();
+                m_transform = transform;
+        }
+
+        void Start ()
+        {
+                rigibody.constraints = RigidbodyConstraints.FreezeRotation;
+                rigibody.useGravity = false;
+        }
+
+
+        void Update ()
+        {
+                planetGravity.AddGravity( m_transform );
+        }
+}
